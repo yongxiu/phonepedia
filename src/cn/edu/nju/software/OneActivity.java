2b3a352fce387b862d2caea4657facf55242cc55
yongxiu <cuiyongxiu@gmail.com>
2010-10-26 15:10:09 +0000

    搜索的初步实现

--- src/cn/edu/nju/software/OneActivity.java
+++ src/cn/edu/nju/software/OneActivity.java
@@ -3,12 +3,15 @@ package cn.edu.nju.software;
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.edu.nju.software.adapter.SearchAdapter;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -17,6 +20,7 @@ public class OneActivity extends Activity {
 
 	private ListView listView;
 	private ImageButton searchButton;
+	private AutoCompleteTextView myAutoCompleteTextView;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,6 +30,12 @@ public class OneActivity extends Activity {
 				android.R.layout.simple_expandable_list_item_1, getData()));
 		searchButton = (ImageButton) findViewById(R.id.ImageButton01);
 		searchButton.setOnClickListener(new SearchListener());
+		myAutoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.AutoCompleteTextView01);
+		
+		/* new一个自己实现做的BaseAdapter */
+	    SearchAdapter adapter = new SearchAdapter(this);
+
+	    myAutoCompleteTextView.setAdapter(adapter);
 	}
 
 	private List<String> getData(){
@@ -46,6 +56,8 @@ public class OneActivity extends Activity {
 
 		public void onClick(View v) {
 			Intent intent = new Intent();
+			System.out.println(myAutoCompleteTextView.getText().toString());
+			intent.putExtra("search", myAutoCompleteTextView.getText().toString());
 			intent.setClass(OneActivity.this, PediaActivity.class);
 			OneActivity.this.startActivity(intent);
 		}
